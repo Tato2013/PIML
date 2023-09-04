e5b8242ea0e7d2b2e65d3259783e1662d8b8328f
Tato2013 <101934772+Tato2013@users.noreply.github.com>
2023-09-03 21:35:30 -0300

    correciones

--- main.py
+++ main.py
@@ -1,16 +1,68 @@
 import pandas as pd
 import numpy as np
 from fastapi import FastAPI, HTTPException, Path
-import numpy as np
+from sklearn.neighbors import NearestNeighbors
+from fastapi.responses import JSONResponse
 from sklearn.metrics.pairwise import cosine_similarity
+from datetime import datetime
 
 app = FastAPI()
 
+
+    
+    
 #DataFrame a utilizar
 reviews=pd.read_parquet('reviews.parquet')
 steam=pd.read_parquet('steam.parquet')
 item=pd.read_parquet('item.parquet')
+play_time=pd.read_parquet('play_time.parquet')
 
+# Variables globales
+
+features = None
+nn = None
+game_ids = None
+
+@app.on_event("startup")
+async def load_data():
+    global df_steam, features, nn, game_ids
+
+    
+    # Selecciona las características relevantes para la similitud (por ejemplo, action, adventure, strategy)
+    features = steam[['action', 'adventure', 'strategy','earlyaccess','indie','free to play']]  # Añade todas las características relevantes
+    
+    # Crea un modelo NearestNeighbors basado en similitud coseno
+    nn = NearestNeighbors(n_neighbors=6, metric='cosine')
+    nn.fit(features)
+    
+    # Almacena los IDs de los juegos para acceder fácilmente
+    game_ids = steam['item_id'].tolist()
+
+def recomendacion_juego(id_producto, similarity_matrix, num_recomendaciones=5):
+    
+    indice_juego_consulta = obtener_indice_por_id(id_producto)
+    
+    if indice_juego_consulta is None:
+        return None  
+    
+    # Obtén las puntuaciones de similitud del juego de consulta con todos los juegos
+    similitudes_con_juego_consulta = similarity_matrix[indice_juego_consulta]
+    
+    # Enumera los juegos y sus puntuaciones de similitud con respecto al juego de consulta
+    juegos_y_similitudes = list(enumerate(similitudes_con_juego_consulta))
+    
+    # Ordena la lista por similitud en orden descendente
+    juegos_y_similitudes_ordenados = sorted(juegos_y_similitudes, key=lambda x: x[1], reverse=True)
+    
+    # Selecciona los juegos más similares (excluyendo el juego de consulta en sí)
+    juegos_recomendados = juegos_y_similitudes_ordenados[1:num_recomendaciones + 1]
+    
+    # Obtiene los nombres de los juegos recomendados en una lista
+    nombres_recomendados = [obtener_nombre_por_indice(indice) for indice, _ in juegos_recomendados]
+    
+    return nombres_recomendados
+
+# Función auxiliar para obtener el nombre del juego por su índice
 def obtener_nombre_por_indice(indice):
     # Implementa la lógica para obtener el nombre del juego por su índice
     # Debes ajustar esto según cómo estén estructurados tus datos
@@ -18,6 +70,13 @@ def obtener_nombre_por_indice(indice):
     # Ejemplo: Supongamos que los nombres de los juegos están en una columna 'appname'
     return steam.iloc[indice]['appname']
 
+# Función auxiliar para obtener el índice del juego por su ID de producto
+def obtener_indice_por_id(id_producto):
+    # Implementa la lógica para obtener el índice del juego por su ID de producto
+    # Debes ajustar esto según cómo estén estructurados tus datos
+    
+    # Ejemplo: Supongamos que los IDs de producto están en una columna 'productid'
+    return steam[steam['item_id'] == id_producto].index[0]
 
 #Mensaje
 
@@ -37,13 +96,7 @@ def userdata(user_id: str):
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
 
     # Filtrar los datos relevantes del usuario en df_item y hacer el join con steam
-    user_data = item[item['user_id'] == user_id]
-    
-    # Verificar que item_id es una lista
-    if not isinstance(user_data['item_id'].iloc[0], list):
-        raise HTTPException(status_code=500, detail="item_id debe ser una lista")
-
-    user_data = user_data.explode('item_id').merge(
+    user_data = item[item['user_id'] == user_id].explode('item_id').merge(
         steam[['item_id', 'price']], on='item_id', how='left')
     
     # Calcular el total gastado por el usuario
@@ -63,21 +116,28 @@ def userdata(user_id: str):
     
     # Crear el diccionario con los resultados
     result_dict = {
-        'user_id': user_id,  # Agregamos el user_id a la respuesta
-        'total_spent': total_spent,
-        'total_items_bought': total_items_bought,
-        'recommendation_percentage': recommendation_percentage
+        'total_spent': int(total_spent),
+        'total_items_bought': int(total_items_bought),
+        'recommendation_percentage': float(recommendation_percentage)
     }
     
     return result_dict
 
 
-@app.get('/reviews/{start_date}/{end_date}')
 
+
+@app.get('/reviews/{start_date}/{end_date}')
 def countreviews(start_date: str, end_date: str):
-    # Convertir las fechas de inicio y fin a objetos datetime
-    start_date = pd.to_datetime(start_date)
-    end_date = pd.to_datetime(end_date)
+    # Verificar el formato de las fechas ingresadas
+    
+    date_format = "%Y-%m-%d"
+    try:
+        start_date = datetime.strptime(start_date, date_format)
+        end_date = datetime.strptime(end_date, date_format)
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Formato de fecha incorrecto. Utilice 'YYYY-MM-DD'.")
+    
+    reviews['posted'] = pd.to_datetime(reviews['posted'], format='%Y-%m-%d')
     
     # Filtrar las reviews entre las fechas dadas
     filtered_reviews = reviews[(reviews['posted'] >= start_date) & (reviews['posted'] <= end_date)]
@@ -86,7 +146,11 @@ def countreviews(start_date: str, end_date: str):
     unique_users = filtered_reviews['user_id'].nunique()
     
     # Calcular el porcentaje de recomendación
-    recommendation_percentage = (filtered_reviews['recommend'].sum() / len(filtered_reviews)) * 100
+    total_reviews = len(filtered_reviews)
+    if total_reviews > 0:
+        recommendation_percentage = (filtered_reviews['recommend'].sum() / total_reviews) * 100
+    else:
+        recommendation_percentage = 0.0
     
     result = {
         'unique_users': unique_users,
@@ -98,58 +162,65 @@ def countreviews(start_date: str, end_date: str):
 
 
 @app.get('/steam/{genero}')
+def genre(genero: str):
+    # Asegúrate de que 'steam' e 'item' sean accesibles dentro de esta función
+    global steam, play_time
 
-def genre(género: str):
-    # Asegúrate de que 'steam' e 'item' son accesibles dentro de esta función
-    global steam, item
+    # Convierte la columna 'item_id' de 'steam' y 'play_time' al mismo tipo de datos (por ejemplo, str)
+    steam['item_id'] = steam['item_id'].astype(str)
+    play_time['item_id'] = play_time['item_id'].astype(str)
 
-    # Unir los DataFrames en base a la columna 'item_id'
-    df_merged = pd.merge(steam, item, on='item_id')
+    # Filtra las filas en 'steam' donde el género especificado tiene un valor de 1
+    steam_filtrado = steam[steam[genero] == 1]
 
-    # Calcula la suma total de 'playtime_forever' para cada género
-    playtime_por_género = df_merged.loc[:, 'action':'web publishing'].multiply(df_merged['playtime_forever'], axis="index").sum()
+    # Verifica si hay datos disponibles para el género especificado
+    if steam_filtrado.empty:
+        return {genero: "No encontrado en los datos"}
 
-    # Crea un ranking de géneros por 'playtime_forever'
-    ranking = playtime_por_género.sort_values(ascending=False)
+    # Realiza la unión con 'play_time' en base a la columna 'item_id'
+    df_merged = pd.merge(steam_filtrado, play_time, on='item_id')
 
-    # Encuentra el puesto del género dado en el ranking
-    puesto = ranking.index.get_loc(género) + 1
+    # Calcula la suma total de 'playtime_forever' para el género especificado
+    total_playtime = df_merged['playtime_forever'].sum()
 
-    # Devuelve un diccionario con el género y su puesto
-    return {género: puesto}
+    # Devuelve un diccionario con el género y su suma total de playtime
+    return {genero: total_playtime}
 
 
-
-@app.get('/genero/{genero}')
-
+@app.get('/userforgenre/{genero}')
 def userforgenre(genero: str):
-    # Filtrar el DataFrame 'steam' por el género deseado
+    # Filtra el DataFrame 'steam' por el género deseado
     genre_items = steam[steam[genero] == 1]
     
-    # Merge de 'genre_items' con 'item' para obtener la información de PlayTimeForever y user_url
-    merged_data = genre_items.merge(item[['item_id', 'playtime_forever', 'user_id', 'user_url']], left_on='item_id', right_on='item_id', how='inner')
+    genre_items['item_id'] = genre_items['item_id'].astype(str)
+    play_time['item_id'] = play_time['item_id'].astype(str)
+    # Fusiona 'genre_items' con 'play_time' utilizando 'item_id' como clave de fusión
+    merged_steam_playtime = genre_items.merge(play_time, on='item_id', how='inner')
     
-    # Agrupar por 'user_id' y sumar las horas de juego
-    user_playtime = merged_data.groupby('user_id')['playtime_forever'].sum().reset_index()
+    # Fusiona el resultado anterior con 'item' utilizando 'user_id' como clave de fusión
+    final_merged_data = merged_steam_playtime.merge(item[['user_id', 'user_url']], on='user_id', how='inner')
     
-    # Ordenar los datos por PlayTimeForever en orden descendente y obtener el top 5
+    # Agrupa por 'user_id' y suma las horas de juego
+    user_playtime = final_merged_data.groupby('user_id')['playtime_forever'].sum().reset_index()
+    
+    # Ordena los datos por PlayTimeForever en orden descendente y obtiene el top 5
     top_users = user_playtime.sort_values(by='playtime_forever', ascending=False).head(5)
     
-    # Agregar las URLs de los usuarios al resultado final
+    # Agrega las URLs de los usuarios al resultado final
     top_users = top_users.merge(item[['user_id', 'user_url']], on='user_id', how='left')
     
-    # Convertir el DataFrame a un diccionario y devolverlo
+    # Convierte el DataFrame a un diccionario y devuélvelo
     return top_users[['user_id', 'user_url', 'playtime_forever']].to_dict('records')
 
 
 
-
-@app.get('/developer/{desarrolador}')
-
-
+@app.get('/developer/{desarrollador}')
 def developer(desarrollador: str):
     # Filtrar el DataFrame 'steam' por el desarrollador deseado y crear una copia
     dev_items = steam[steam['developer'] == desarrollador].copy()
+    
+    # Convertir la columna 'releasedate' a tipo datetime
+    dev_items['releasedate'] = pd.to_datetime(dev_items['releasedate'])
     
     # Crear una columna 'is_free' que es True si el precio es 0 y False en caso contrario
     dev_items['is_free'] = dev_items['price'] == 0
@@ -191,29 +262,33 @@ def sentiment_analysis(año: int):
 
 
 
-@app.get('/Modelo/{genero}')
+def map_game_ids_to_names(game_ids):
+    # Utiliza tu DataFrame de juegos "steam" para mapear los IDs a nombres de juegos
+    game_names = []
+    for game_id in game_ids:
+        game_name = steam[steam['item_id'] == game_id]['appname'].values[0]
+        game_names.append(game_name)
+    return game_names
 
-
-def recomendacion_juego(id_producto, similarity_matrix, num_recomendaciones=5):
-    # Encuentra el índice del juego correspondiente al ID de producto
-    indice_juego_consulta = obtener_indice_por_id(id_producto)
+@app.get('/recomendacion/{game_id}')
+def recomendacion(game_id: int):
+    '''Ingresa el ID de un juego y obtén una lista de 5 juegos recomendados similares.'''
     
-    if indice_juego_consulta is None:
-        return None  # El juego no se encontró en la base de datos
-    
-    # Obtén las puntuaciones de similitud del juego de consulta con todos los juegos
-    similitudes_con_juego_consulta = similarity_matrix[indice_juego_consulta]
-    
-    # Enumera los juegos y sus puntuaciones de similitud con respecto al juego de consulta
-    juegos_y_similitudes = list(enumerate(similitudes_con_juego_consulta))
-    
-    # Ordena la lista por similitud en orden descendente
-    juegos_y_similitudes_ordenados = sorted(juegos_y_similitudes, key=lambda x: x[1], reverse=True)
-    
-    # Selecciona los juegos más similares (excluyendo el juego de consulta en sí)
-    juegos_recomendados = juegos_y_similitudes_ordenados[1:num_recomendaciones + 1]
-    
-    # Obtiene los nombres de los juegos recomendados en forma de un diccionario
-    recomendaciones = {i + 1: obtener_nombre_por_indice(indice) for i, (indice, _) in enumerate(juegos_recomendados)}
-    
-    return recomendaciones
+    # Verifica si el ID de juego ingresado se encuentra en el DataFrame
+    if game_id not in game_ids:
+        return 'El juego no se encuentra en la base de datos.'
+    else:
+        # Encuentra el índice correspondiente al ID del juego
+        index = game_ids.index(game_id)
+        
+        # Calcula la similitud entre juegos y obtén los juegos más similares
+        similarities = cosine_similarity([features.iloc[index]], features)
+        similar_games_indices = similarities.argsort()[0][-6:-1]  # Excluye el juego en sí mismo
+        
+        # Obtiene los IDs de los juegos recomendados
+        recommended_game_ids = [game_ids[i] for i in similar_games_indices]
+        
+        # Mapea los IDs a nombres de juegos
+        recommended_game_names = map_game_ids_to_names(recommended_game_ids)
+        
+        return recommended_game_names
